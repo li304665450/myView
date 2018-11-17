fc7f1a16ccfcf9b757d520590c5373bf11eafcd9
lilei <15600017090@163.com>
2018-11-17 18:36:25 +0800

    server.js

---
+++ build/webpack.config.server.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const webpack = require('webpack')
+const baseConfig = require('./webpack.config.base')
+const marage = require('webpack-merge')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const VueServerPlugin = require('vue-server-renderer/server-plugin')
+
+let config
+
+// development  开发环境
+config = marage(baseConfig, {
+  target: 'node',
+  entry: path.join(__dirname, '../src/server-entry.js'),
+  devtool: 'source-map',
+  output: {
+    libraryTarget: 'commonjs2',
+    filename: 'server-entry.js',
+    path: path.join(__dirname, '../server-build')
+  },
+  externals: Object.keys(require('../package.json').dependencies),
+  module: {
+    rules: [{
+      test: /\.styl(us)?$/,
+      use: ExtractPlugin.extract({
+        fallback: 'vue-style-loader',
+        use: [
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'stylus-loader'
+        ]
+      })
+    }]
+  },
+  plugins: [
+    new ExtractPlugin('styles.[hash:8].css'),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.VUE_ENV': 'server'
+    }),
+    new VueServerPlugin()
+  ]
+})
+
+module.exports = config
