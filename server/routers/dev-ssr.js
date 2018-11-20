ca3d04373114d6a566b3ccc6385f83371d1ec9ff
lilei <15600017090@163.com>
2018-11-20 10:00:35 +0800

    server

--- server/routers/dev-ssr.js
+++ server/routers/dev-ssr.js
@@ -6,6 +6,7 @@ const MemoryFS = require('memory-fs')
 const webpack = require('webpack')
 const VueServerRenderer = require('vue-server-renderer')
 
+const serverRender = require('./server-render')
 const serverConfig = require('../../build/webpack.config.server')
 
 const serverCompiler = webpack(serverConfig)
@@ -13,21 +14,22 @@ const mfs = MemoryFS()
 serverCompiler.outputFileSystem = mfs
 
 let bundle
-serverCompiler.watch({}, (err, stats) = {
+serverCompiler.watch({}, (err, stats) => {
   if (err) throw err
-    stats = stats.toJson()
-    stats.erros.forEach(err => console.log(err))
-    stats.hasWarnings.forEach(warn => console.warn(err))
+  stats = stats.toJson()
+  stats.errors.forEach(err => console.log(err))
+  stats.warnings.forEach(warn => console.warn(err))
 
-    const bundlePath = path.join(
-      serverConfig.output.path,
-      'vue-ssr-server-bundle.json'
-    )
-    bundel = JSON.parse(mfs.readFileSync(bundlePath, 'utf-8'))
+  const bundlePath = path.join(
+    serverConfig.output.path,
+    'vue-ssr-server-bundle.json'
+  )
+  bundle = JSON.parse(mfs.readFileSync(bundlePath, 'utf-8'))
+  console.log('new bundle generated')
 })
 
 const handleSSR = async (ctx) => {
-  if (bundle) {
+  if (!bundle) {
     ctx.body = '你等一会，别着急.....'
     return
   }
@@ -39,12 +41,20 @@ const handleSSR = async (ctx) => {
   const clientManifest = clientManifestResp()
 
   const template = fs.readFileSync(
-    path.join(__dirname, '../server.template.ejs')
+    path.join(__dirname, '../server.template.ejs'),
+    'utf-8'
   )
 
   const renderer = VueServerRenderer
-  .createBundleRenderer(bundle, {
-    inject: false,
-    clientManifest
-  })
+    .createBundleRenderer(bundle, {
+      inject: false,
+      clientManifest
+    })
+
+  await serverRender(ctx, renderer, template)
 }
+
+const router = new Router()
+router.get('*', handleSSR)
+
+module.exports = router
--- src/config/routes.js
+++ src/config/routes.js
@@ -7,7 +7,8 @@ export default [
     redirect: '/app'
   },
   {
-    path: '/app/:id',
+    // path: '/app/:id',
+    path: '/app',
     props: true,
     component: () => import('../views/todo/todo.vue'),
     name: 'app',
